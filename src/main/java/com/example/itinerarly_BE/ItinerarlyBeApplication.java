3e75caf1037f79b544c96743f3a92b9308c7564a
Heisen47 <iamjeet47@gmail.com>
2025-05-08 13:03:25 +0530

    first commit

---
+++ src/main/java/com/example/itinerarly_BE/ItinerarlyBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.itinerarly_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItinerarlyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItinerarlyBeApplication.class, args);
+	}
+
+}
