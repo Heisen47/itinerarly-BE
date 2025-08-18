3fe49c8b8de1ad991ea32a4b4363267e58e85c78
Heisen47 <iamjeet47@gmail.com>
2025-08-18 16:03:21 +0530

    add: Header-based Authentication & Multiple Cookie Formats 2

--- src/main/java/com/example/itinerarly_BE/config/SessionConfig.java
+++ src/main/java/com/example/itinerarly_BE/config/SessionConfig.java
@@ -5,8 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.server.CookieSameSiteSupplier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.web.http.CookieSerializer;
-import org.springframework.session.web.http.DefaultCookieSerializer;
 
 @Configuration
 public class SessionConfig {
@@ -14,25 +12,8 @@ public class SessionConfig {
     private static final Logger logger = LoggerFactory.getLogger(SessionConfig.class);
 
     @Bean
-    public CookieSerializer cookieSerializer() {
-        logger.info("Configuring cookie serializer for cross-domain support");
-
-        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
-        serializer.setCookieName("JSESSIONID");
-        serializer.setCookiePath("/");
-        serializer.setDomainNamePattern("^.+?\\.(\\w+\\.[a-z]+)$");
-        serializer.setHttpOnly(false);
-        serializer.setSecure(true);
-        serializer.setSameSite("None");
-        serializer.setUseSecureCookie(true);
-        serializer.setCookieMaxAge(86400); // 24 hours
-
-        logger.info("Cookie serializer configured for cross-domain cookies with SameSite=None and Secure=true");
-        return serializer;
-    }
-
-    @Bean
     public CookieSameSiteSupplier cookieSameSiteSupplier() {
+        logger.info("Configuring cookie SameSite attribute for cross-domain support");
         return CookieSameSiteSupplier.ofNone();
     }
 }
